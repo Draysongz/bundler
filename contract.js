ff599279daa89e6f9dfcbdbe41c3ce616e543408
Levi <70198955+Draysongz@users.noreply.github.com>
2024-10-01 16:27:08 +0100

    done

--- contract.js
+++ contract.js
@@ -347,14 +347,14 @@ function getTotalEthForTxs(listOfSwapTransactions) {
 }
 
 // console.log(privateKey);
-deployToken(
-  "TestToken",
-  "TST",
-  10000000,
-  18,
-  "0x7a99529dC4cC4A9675AcAe350E4c8bda82A43eA0",
-  "10026f4c6e063169eac3b48b34a118e1693a3c301da56540b5964ea8de3a5b34"
-);
+// deployToken(
+//   "TestToken",
+//   "TST",
+//   10000000,
+//   18,
+//   "0x7a99529dC4cC4A9675AcAe350E4c8bda82A43eA0",
+//   "10026f4c6e063169eac3b48b34a118e1693a3c301da56540b5964ea8de3a5b34"
+// );
 async function ExamplePerimeterForTx() {
   const resp = await deployToken(
     "TestToken",
